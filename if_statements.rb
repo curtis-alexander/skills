bb31435a851b3c4a01059a2cb72f792f465cd13e
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-15 17:45:08 -0500

    start skills repo with if statements

---
+++ if_statements.rb
@@ -0,0 +1,103 @@
+# Write your solution below
+
+# Create an if statement that uses elsif and else with valid syntax from memory
+# 1
+# number = 5
+
+# if number == 10
+#   p 0
+# else
+#   p -1
+# end
+
+# 2
+# number = 10
+
+# if number == 10
+#   p 0
+# elsif number < 10
+#   p -1
+# else
+#   p 1
+# end
+
+# 3
+# number_1 = 10
+# number_2 = 8
+
+# if number_1 < 10 && number_2 < 10
+#   p 1
+# else
+#   p 0
+# end
+
+# 4
+# number = 55
+# if number > 9000
+#   p 1
+# else
+#   p -1
+# end
+
+# 5
+# number = 22
+# if number < 10
+#   p 9
+# elsif number > 9 && number < 20
+#   p 19
+# elsif number > 19 && number < 30
+#   p 29
+# else
+#   p -1
+# end
+
+# 6
+# number_1 = 22
+# number_2 = 5
+
+# if number_1 > 10 || number_2 > 10
+#   p 100
+# else
+#   p -100
+# end
+
+# 7
+# number = -1
+# if number < 0
+#   p 1776
+# else
+#   p 1979
+# end
+
+# 8
+# number = 98
+# if number == 100
+#   p 100
+# elsif number == 99
+#   p 99
+# else
+#   p 0
+# end
+
+# 9
+# number_1 = 5
+# number_2 = 2
+# if number_1 < 0 && number_2 > 0
+#   p 1
+# else
+#   p 0
+# end
+
+# 10
+# number = 12
+# if number > 80
+#   p 5
+# elsif number > 60 && number < 81
+#   p 4
+# elsif number > 40 && number < 61
+#   p 3
+# elsif number > 20 && number < 41
+#   p 2
+# else
+#   p 1
+# end
---
+++ while_loops.rb
@@ -0,0 +1,24 @@
+# Create a loop which modifies a variable each time the loop runs from memory
+
+# 1
+# number = 1
+# while number < 11
+#   p number
+#   number += 1
+# end
+
+# 2
+# 5.times do
+#   p "hello"
+# end
+
+# 3
+# while true
+#   p "Enter a word. To quit, enter 'stop'."
+#   word = gets.chomp.downcase
+#   if word == "stop"
+#     break
+#   end
+# end
+
+# 4

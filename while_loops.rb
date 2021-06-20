d35709e3f66eef94be4f0353ede0c4fdc97c118b
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-20 08:40:57 -0500

    finished while loops

--- while_loops.rb
+++ while_loops.rb
@@ -2,7 +2,7 @@
 
 # 1
 # number = 1
-# while number < 11
+# while number <= 10
 #   p number
 #   number += 1
 # end
@@ -22,3 +22,54 @@
 # end
 
 # 4
+# number = 0
+# while number <= 100
+#   p number
+#   number += 5
+# end
+
+# 5
+# 10.times do
+#   p 9000
+# end
+
+# 6
+# while true
+#   print "Enter a number less than 10. To quit, enter a number greater than 10. "
+#   number = gets.chomp.to_i
+#   if number > 10
+#     break
+#   elsif number <= 10
+#     p number
+#   end
+# end
+
+# 7
+# number = 50
+# while number <= 70
+#   p number
+#   number += 1
+# end
+
+# 8
+# 144.times do
+#   p "Around the world"
+# end
+
+# 9
+# while true
+#   print "Enter a word. To quit, enter a word with more than 5 letters. "
+#   word = gets.chomp.downcase
+#   if word.length > 5
+#     break
+#   end
+# end
+
+# 10
+number = 1
+while number <= 40
+  if number.even?
+    p number
+  end
+  number += 1
+end
